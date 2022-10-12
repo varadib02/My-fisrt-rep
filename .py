7f90cdea46ec62b5cf78696e31bc2527f6a50f8b
varadib02 <91455828+varadib02@users.noreply.github.com>
2022-10-12 08:02:23 +0200

    math

--- .py
+++ .py
@@ -1,6 +1,14 @@
+import math
+
 #bekeres
 be=input("Kérek szöveget")
 szam=int(input("Kérek számot"))
 #kiíras
 print(f"A bekért szöveg: {be}")
-
+#gyök
+gy=math.sqrt(szam)
+print(f"Gyöke {gy}")
+#hatványozás
+menyivel=int(input("hatványozás (hányadikon): "))
+hatv=math.pow(szam,menyivel)
+print(f"{hatv}")
