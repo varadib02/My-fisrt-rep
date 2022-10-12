b15863f2912d2096f0a6b0211fff8514d47cc366
varadib02 <91455828+varadib02@users.noreply.github.com>
2022-10-12 09:09:39 +0200

    def

--- .py
+++ .py
@@ -1,18 +1,22 @@
 import math
 
+
 #bekeres
-be=input("Kérek szöveget: ")
-szam=int(input("Kérek számot: "))
-#kiíras
-print(f"A bekért szöveg: {be}")
+def bekerse():
+    be=input("Kérek szöveget: ")
+    szam=int(input("Kérek számot: "))
+    #kiíras
+    print(f"A bekért szöveg: {be}")
 
 #gyök
-gy=math.sqrt(szam)
-print(f"Gyöke {gy}")
+def gyok_hatvanyozas():
+    szam = 2
+    gy=math.sqrt(szam)
+    print(f"Gyöke {gy}")
 #hatványozás
-menyivel=int(input("hatványozás (hányadikon): "))
-hatv=math.pow(szam,menyivel)
-print(f"{hatv}")
+    menyivel=int(input("hatványozás (hányadikon): "))
+    hatv=math.pow(szam,menyivel)
+    print(f"{hatv}")
 
 #fix elem bekérése
 #listába tárolás
@@ -22,6 +26,7 @@ for i in range(0,n):
     be_adat=input(f"Kérem az {i+1} elemet: ")
     #listához hozzáadás
     lista.append(be_adat)
-#kiírás adatonként
+#kiírás adatonként, lista végéig 
 for i in range(len(lista)):
     print(lista[i])
+
