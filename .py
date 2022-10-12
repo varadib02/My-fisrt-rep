221f456956b5c8fb06ef75a86e00116013f62888
varadib02 <91455828+varadib02@users.noreply.github.com>
2022-10-12 08:19:16 +0200

    for ciklus

--- .py
+++ .py
@@ -1,10 +1,11 @@
 import math
 
 #bekeres
-be=input("Kérek szöveget")
-szam=int(input("Kérek számot"))
+be=input("Kérek szöveget: ")
+szam=int(input("Kérek számot: "))
 #kiíras
 print(f"A bekért szöveg: {be}")
+
 #gyök
 gy=math.sqrt(szam)
 print(f"Gyöke {gy}")
@@ -12,3 +13,15 @@ print(f"Gyöke {gy}")
 menyivel=int(input("hatványozás (hányadikon): "))
 hatv=math.pow(szam,menyivel)
 print(f"{hatv}")
+
+#fix elem bekérése
+#listába tárolás
+n=5
+lista = []
+for i in range(0,n):
+    be_adat=input(f"Kérem az {i+1} elemet: ")
+    #listához hozzáadás
+    lista.append(be_adat)
+#kiírás adatonként
+for i in range(len(lista)):
+    print(lista[i])
